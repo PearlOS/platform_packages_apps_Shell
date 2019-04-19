182b4369fc765cf4112055eaadbe7a3ae24f91dc
Ovenoboyo <ovenoboyo@gmail.com>
2019-04-19 20:21:20 +0530

    [2/2]: Add customisable alignment for Q style clock No shit Sherlock

--- src/com/shell/extras/tabs/Lockscreen.java
+++ src/com/shell/extras/tabs/Lockscreen.java
@@ -21,6 +21,7 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.ListPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.settings.R;
@@ -29,10 +30,23 @@ import com.android.internal.logging.nano.MetricsProto;
 
 public class Lockscreen extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
+    private ListPreference clockAlign, clockSelect;
+    private PreferenceScreen preferenceScreen;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lockscreen);
+        preferenceScreen = getPreferenceScreen();
+        clockSelect = (ListPreference) findPreference("lockscreen_clock_selection");
+        clockAlign = (ListPreference) findPreference("lockscreen_text_clock_align");
+        clockSelect.setOnPreferenceChangeListener(this);
+        String value = clockSelect.getValue();
+        if (value.equals("6")) {
+            preferenceScreen.addPreference(clockAlign);
+        } else {
+            preferenceScreen.removePreference(clockAlign);
+        }
     }
 
     @Override
@@ -48,7 +62,12 @@ public class Lockscreen extends SettingsPreferenceFragment implements Preference
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
-        return false;
+        if (objValue.toString().equals("6")) {
+            preferenceScreen.addPreference(clockAlign);
+        } else {
+            preferenceScreen.removePreference(clockAlign);
+        }
+        return true;
     }
 
 
