6bf63746070f9b43e527f1ce32033b26475fe503
Decad3nce <Decad3nce@users.noreply.github.com>
2019-12-04 19:08:26 +0000

    HeadsUp blacklists (2/2)

--- src/com/shell/settings/fragments/NotificationsSettings.java
+++ src/com/shell/settings/fragments/NotificationsSettings.java
@@ -15,18 +15,35 @@
  */
 package com.shell.settings.fragments;
 
+import android.content.Context;
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class NotificationsSettings extends SettingsPreferenceFragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shell.settings.preferences.GlobalSettingMasterSwitchPreference;
+
+public class NotificationsSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     private Preference mChargingLeds;
+    private static final String HEADS_UP_NOTIFICATIONS_ENABLED = "heads_up_notifications_enabled";
+
+    private GlobalSettingMasterSwitchPreference mHeadsUpEnabled;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -34,6 +51,7 @@ public class NotificationsSettings extends SettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.shell_settings_notifications);
         PreferenceScreen prefScreen = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
 
         mChargingLeds = (Preference) findPreference("charging_light");
         if (mChargingLeds != null
@@ -41,6 +59,28 @@ public class NotificationsSettings extends SettingsPreferenceFragment {
                         com.android.internal.R.bool.config_intrusiveBatteryLed)) {
             prefScreen.removePreference(mChargingLeds);
         }
+
+        mHeadsUpEnabled = (GlobalSettingMasterSwitchPreference) findPreference(HEADS_UP_NOTIFICATIONS_ENABLED);
+        mHeadsUpEnabled.setOnPreferenceChangeListener(this);
+        int headsUpEnabled = Settings.Global.getInt(getContentResolver(),
+                HEADS_UP_NOTIFICATIONS_ENABLED, 1);
+        mHeadsUpEnabled.setChecked(headsUpEnabled != 0);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHeadsUpEnabled) {
+            boolean value = (Boolean) newValue;
+            Settings.Global.putInt(getContentResolver(),
+		            HEADS_UP_NOTIFICATIONS_ENABLED, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
